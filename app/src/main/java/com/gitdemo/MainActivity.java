29e0440470a25ecf98a8785d5713d06a78c0ac50
shh <652475101@qq.com>
2017-06-09 11:09:31 +0800

    no message

--- app/src/main/java/com/gitdemo/MainActivity.java
+++ app/src/main/java/com/gitdemo/MainActivity.java
@@ -2,8 +2,11 @@ package com.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
