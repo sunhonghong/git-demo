85a5060b2e1c4998c315f06aa0c89819544f1408
shh <652475101@qq.com>
2017-06-19 12:29:09 +0800

    no message

--- app/src/main/java/com/gitdemo/MainActivity.java
+++ app/src/main/java/com/gitdemo/MainActivity.java
@@ -1,16 +1,86 @@
 package com.gitdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
+    ImageView ivPhoto;
+    Bitmap bitMap;
+    TextView tvTj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bitMap = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+
+        Drawable newBitmapDrawable = new BitmapDrawable(makeRoundCorner(bitMap));
+        ivPhoto = (ImageView) findViewById(R.id.ivPhoto);
+        tvTj = (TextView) findViewById(R.id.tvTj);
+        tvTj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TjActivity.openActivity(MainActivity.this);
+            }
+        });
+        ivPhoto.setBackground(newBitmapDrawable);
+    }
+
+    public static Bitmap makeRoundCorner(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int left = 0, top = 0, right = width, bottom = height;
+        float roundPx = height / 2;
+        if (width > height) {
+            left = (width - height) / 2;
+            top = 0;
+            right = left + height;
+            bottom = height;
+        } else if (height > width) {
+            left = 0;
+            top = (height - width) / 2;
+            right = width;
+            bottom = top + width;
+            roundPx = width / 2;
+        }
+
+        Bitmap output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        int color = 0xff424242;
+        Paint paint = new Paint();
+        Rect rect = new Rect(left, top, right, bottom);
+        RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (!(bitMap.isRecycled())){
+            bitMap.recycle();
+        }
     }
 }
--- app/src/main/java/com/gitdemo/TjActivity.java
+++ app/src/main/java/com/gitdemo/TjActivity.java
@@ -1,8 +1,24 @@
 package com.gitdemo;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by SHH on 2017/6/14.
  */
 
-class TjActivity {
+public class TjActivity extends Activity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tj);
+    }
+    public static void openActivity(Context context){
+        Intent intent = new Intent(context,TjActivity.class);
+        context.startActivity(intent);
+    }
 }
